83e08699e78a01a8980784b35e9338310a22ef52
Simon Cruanes <simon.cruanes.2007@m4x.org>
2014-02-17 10:37:48 +0100

    a benchmark for Gen.persistent

---
+++ bench/bench_persistent.ml
@@ -0,0 +1,79 @@
+module MList = struct
+  type 'a t = 'a node option ref
+  and 'a node = {
+    content : 'a;
+    mutable prev : 'a node;
+    mutable next : 'a node;
+  }
+
+  let create () = ref None
+
+  let is_empty d =
+    match !d with
+    | None -> true
+    | Some _ -> false
+
+  let push_back d x =
+    match !d with
+    | None ->
+      let rec elt = {
+        content = x; prev = elt; next = elt; } in
+      d := Some elt
+    | Some first ->
+      let elt = { content = x; next=first; prev=first.prev; } in
+      first.prev.next <- elt;
+      first.prev <- elt
+
+  (* conversion to gen *)
+  let to_gen d =
+    fun () ->
+      match !d with
+      | None -> (fun () -> None)
+      | Some first ->
+        let cur = ref first in (* current element of the list *)
+        let stop = ref false in (* are we done yet? *)
+        fun () ->
+          if !stop then None
+          else begin
+            let x = (!cur).content in
+            cur := (!cur).next;
+            (if !cur == first then stop := true); (* EOG, we made a full cycle *)
+            Some x
+          end
+end
+
+(** Store content of the generator in an enum *)
+let persistent_mlist gen =
+  let l = MList.create () in
+  Gen.iter (MList.push_back l) gen;
+  MList.to_gen l
+
+let bench_mlist n =
+  for i = 0 to 100 do
+    let _ = persistent_mlist Gen.(1 -- n) in
+    ()
+  done
+
+let bench_naive n =
+  for i = 0 to 100 do
+    let l = Gen.to_rev_list Gen.(1 -- n) in
+    let _ = Gen.Restart.of_list (List.rev l) in
+    ()
+  done
+
+let bench_current n =
+  for i = 0 to 100 do
+    let _ = Gen.persistent Gen.(1 -- n) in
+    ()
+  done
+
+let () =
+  let n = 100_000 in
+  let res = Benchmark.throughputN 5
+    [ "mlist", bench_mlist, n
+    ; "naive", bench_naive, n
+    ; "current", bench_current, n
+    ]
+  in Benchmark.tabulate res
+
+(* vim:Use benchmark: *)
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 4105e33a835617b51bc073ab73b3a516) *)
+(* DO NOT EDIT (digest: 2eeb3eccfbfc1ec34359ab71ca11cd08) *)
 module OASISGettext = struct
 (* # 22 "src/oasis/OASISGettext.ml" *)
 
@@ -368,7 +368,11 @@ module MyOCamlbuildFindlib = struct
           flag ["ocaml"; "pkg_threads"; "compile"] (S[A "-thread"]);
           flag ["ocaml"; "pkg_threads"; "doc"] (S[A "-I"; A "+threads"]);
           flag ["ocaml"; "pkg_threads"; "link"] (S[A "-thread"]);
-          flag ["ocaml"; "pkg_threads"; "infer_interface"] (S[A "-thread"])
+          flag ["ocaml"; "pkg_threads"; "infer_interface"] (S[A "-thread"]);
+          flag ["ocaml"; "package(threads)"; "compile"] (S[A "-thread"]);
+          flag ["ocaml"; "package(threads)"; "doc"] (S[A "-I"; A "+threads"]);
+          flag ["ocaml"; "package(threads)"; "link"] (S[A "-thread"]);
+          flag ["ocaml"; "package(threads)"; "infer_interface"] (S[A "-thread"]);
 
       | _ ->
           ()
@@ -547,7 +551,7 @@ module MyOCamlbuildBase = struct
 end
 
 
-# 550 "myocamlbuild.ml"
+# 554 "myocamlbuild.ml"
 open Ocamlbuild_plugin;;
 let package_default =
   {
@@ -560,6 +564,6 @@ let package_default =
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default package_default;;
 
-# 564 "myocamlbuild.ml"
+# 568 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
