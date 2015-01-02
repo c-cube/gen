3a790e9274ca84a98732a6a94ee45c646bed6a97
carm <carmelo.piccione@gmail.com>
2015-01-02 13:38:56 -0500

    add @since annot to fold_while

--- gen_intf.ml
+++ gen_intf.ml
@@ -143,7 +143,8 @@ module type S = sig
   val fold_while : ('a -> 'b -> 'a * [`Stop | `Continue]) -> 'a -> 'b t -> 'a * 'b t
     (** Fold elements until (['a, `Stop]) is indicated by the accumulator.
         The initial generator itself should not be used anymore, only the result
-        of [fold_while] *)
+        of [fold_while]
+        @since NEXT_RELEASE *)
 
   val drop_while : ('a -> bool) -> 'a t -> 'a t
     (** Drop elements while they satisfy the predicate. The initial generator
