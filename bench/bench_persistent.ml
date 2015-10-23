1180bbdc54285b67d6271fc02ff4b8ae2145d8ea
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-10-23 15:57:57 +0200

    update benchmarks to use Benchmark.Tree

--- bench/bench_persistent.ml
+++ bench/bench_persistent.ml
@@ -1,4 +1,6 @@
 
+module B = Benchmark
+
 let _sum g =
   Gen.Restart.fold (+) 0 g
 
@@ -54,105 +56,93 @@ let persistent_mlist gen =
   MList.to_gen l
 
 let bench_mlist n =
-  for _i = 0 to 100 do
-    let g = persistent_mlist Gen.(1 -- n) in
-    ignore (_sum g)
-  done
+  let g = persistent_mlist Gen.(1 -- n) in
+  ignore (_sum g)
 
 (** {6 Unrolled mutable list} *)
 module UnrolledList = struct
-  type 'a node =
-    | Nil
-    | Partial of 'a array * int
-    | Cons of 'a array * 'a node ref
+type 'a node =
+  | Nil
+  | Partial of 'a array * int
+  | Cons of 'a array * 'a node ref
 
-  let of_gen gen =
-    let start = ref Nil in
-    let chunk_size = ref 16 in
-    let rec fill prev cur =
-      match cur, gen() with
-      | Partial (a,n), None ->
-          prev := Cons (Array.sub a 0 n, ref Nil); ()  (* done *)
-      | _, None -> prev := cur; ()  (* done *)
-      | Nil, Some x ->
-          let n = !chunk_size in
-          if n < 4096 then chunk_size := 2 * !chunk_size;
-          fill prev (Partial (Array.make n x, 1))
-      | Partial (a, n), Some x ->
-          assert (n < Array.length a);
-          a.(n) <- x;
-          if n+1 = Array.length a
-          then begin
-            let r = ref Nil in
-            prev := Cons(a, r);
-            fill r Nil
-          end else fill prev (Partial (a, n+1))
-      | Cons _, _ -> assert false
-    in
-    fill start !start ;
-    !start
-
-  let to_gen l () =
-    let cur = ref l in
-    let i = ref 0 in
-    let rec next() = match !cur with
-    | Nil -> None
-    | Cons (a,l') ->
-        if !i = Array.length a
+let of_gen gen =
+  let start = ref Nil in
+  let chunk_size = ref 16 in
+  let rec fill prev cur =
+    match cur, gen() with
+    | Partial (a,n), None ->
+        prev := Cons (Array.sub a 0 n, ref Nil); ()  (* done *)
+    | _, None -> prev := cur; ()  (* done *)
+    | Nil, Some x ->
+        let n = !chunk_size in
+        if n < 4096 then chunk_size := 2 * !chunk_size;
+        fill prev (Partial (Array.make n x, 1))
+    | Partial (a, n), Some x ->
+        assert (n < Array.length a);
+        a.(n) <- x;
+        if n+1 = Array.length a
         then begin
-          cur := !l';
-          i := 0;
-          next()
-        end else begin
-          let y = a.(!i) in
-          incr i;
-          Some y
-        end
-    | Partial _ -> assert false
-    in
-    next
+          let r = ref Nil in
+          prev := Cons(a, r);
+          fill r Nil
+        end else fill prev (Partial (a, n+1))
+    | Cons _, _ -> assert false
+  in
+  fill start !start ;
+  !start
+
+let to_gen l () =
+  let cur = ref l in
+  let i = ref 0 in
+  let rec next() = match !cur with
+  | Nil -> None
+  | Cons (a,l') ->
+      if !i = Array.length a
+      then begin
+        cur := !l';
+        i := 0;
+        next()
+      end else begin
+        let y = a.(!i) in
+        incr i;
+        Some y
+      end
+  | Partial _ -> assert false
+  in
+  next
 end
 
 (** Store content of the generator in an enum *)
 let persistent_unrolled gen =
-  let l = UnrolledList.of_gen gen in
-  UnrolledList.to_gen l
+let l = UnrolledList.of_gen gen in
+UnrolledList.to_gen l
 
 let bench_unrolled n =
-  for _i = 0 to 100 do
-    let g = persistent_unrolled Gen.(1 -- n) in
-    ignore (_sum g)
-  done
+let g = persistent_unrolled Gen.(1 -- n) in
+ignore (_sum g)
 
 let bench_naive n =
-  for _i = 0 to 100 do
-    let l = Gen.to_rev_list Gen.(1 -- n) in
-    let g = Gen.Restart.of_list (List.rev l) in
-    ignore (_sum g)
-  done
+  let l = Gen.to_rev_list Gen.(1 -- n) in
+  let g = Gen.Restart.of_list (List.rev l) in
+  ignore (_sum g)
 
 let bench_current n =
-  for _i = 0 to 100 do
-    let g = Gen.persistent Gen.(1 -- n) in
-    ignore (_sum g)
-  done
+  let g = Gen.persistent Gen.(1 -- n) in
+  ignore (_sum g)
 
 let bench_current_lazy n =
-  for _i = 0 to 100 do
-    let g = Gen.persistent_lazy Gen.(1 -- n) in
-    ignore (_sum g)
-  done
+  let g = Gen.persistent_lazy Gen.(1 -- n) in
+  ignore (_sum g)
 
 let bench_current_lazy_no_cache n =
-  for _i = 0 to 100 do
-    let g = Gen.persistent_lazy ~caching:false Gen.(1 -- n) in
-    ignore (_sum g)
-  done
+  let g = Gen.persistent_lazy ~max_chunk_size:16 ~caching:false Gen.(1 -- n) in
+  ignore (_sum g)
 
 let () =
-  let bench_n n = 
-    Printf.printf "BENCH for %d\n" n;
-    let res = Benchmark.throughputN 5
+  let open B.Tree in
+  let bench_n n =
+    B.throughputN 2 ~repeat:3
       [ "mlist", bench_mlist, n
       ; "naive", bench_naive, n
       ; "unrolled", bench_unrolled, n
@@ -160,9 +150,12 @@ let () =
       ; "current_lazy", bench_current_lazy, n
       ; "current_lazy_no_cache", bench_current_lazy_no_cache, n
       ]
-    in Benchmark.tabulate res
   in
-  bench_n 100;
-  bench_n 100_000;
-  ()
+  let app_int f n = string_of_int n @> lazy (f n) in
+  let app_ints f l = B.Tree.concat (List.map (app_int f) l) in
+  B.Tree.register (
+    "persistent" @>>
+    app_ints bench_n [100; 1_000; 10_000; 100_000]
+  );
+  B.Tree.run_global ()
 
