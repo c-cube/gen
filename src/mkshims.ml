5b71598f141abcf047c399b816d25a44fd75e9b0
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2023-03-15 14:04:06 +0100

    Drop dune-configurator, bytes dependencies

--- src/mkshims.ml
+++ src/mkshims.ml
@@ -1,5 +1,3 @@
-module C = Configurator.V1
-
 let write_file f s =
   let out = open_out f in
   output_string out s; flush out; close_out out
@@ -9,8 +7,5 @@ let shims_pre_407 = "module Stdlib = Pervasives"
 let shims_post_407 = "module Stdlib = Stdlib"
 
 let () =
-  C.main ~name:"mkshims" (fun c ->
-    let version = C.ocaml_config_var_exn c "version" in
-    let major, minor = Scanf.sscanf version "%u.%u" (fun maj min -> maj, min) in
-    write_file "GenShims_.ml" (if (major, minor) >= (4,7) then shims_post_407 else shims_pre_407);
-    )
+  let major, minor = Scanf.sscanf Sys.ocaml_version "%u.%u" (fun maj min -> maj, min) in
+  write_file "GenShims_.ml" (if (major, minor) >= (4,7) then shims_post_407 else shims_pre_407);
